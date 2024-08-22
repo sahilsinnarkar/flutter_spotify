ef9b60f7cd0f00853be407d55d09c7fa6c86fe7f
Sahil Sinnarkar <sahilsinnarkar@gmail.com>
2024-08-22 11:14:24 +0530

    first commit

--- spotify/lib/main.dart
+++ spotify/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:spotify/core/configs/theme/app_theme.dart';
+import 'package:spotify/presentation/splash/pages/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,11 +15,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Container(),
+      theme: AppTheme.lightTheme,
+      home: const SplashPage(),
     );
   }
 }
